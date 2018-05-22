e3e98b66f8b52b0abe358d86ba3bd457f7b8f503
MWEA\MarkS <mark.sandy@maibornwolff.de>
2018-05-22 11:21:55 +0200

    Add unittest

---
+++ Tests.MarkUnit.NET/SolutionFixture.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarkUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkUnit.Assemblies;
+using Moq;
+
+namespace Tests.MarkUnit
+{
+    [TestClass()]
+    public class SolutionFixture
+    {
+        [TestMethod()]
+        public void WithImmediateCheck_Should_Set_Static_ImmediateCheckField_To_True()
+        {
+            new Solution(new Mock<IAssemblyCollector>().Object).WithImmediateCheck();
+            Assert.IsTrue(Instances.ImmediateCheck);
+        }
+    }
+}
